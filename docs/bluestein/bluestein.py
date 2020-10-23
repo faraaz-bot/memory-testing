39ceb1e57edb96b3367da8808419eefe275613e7
Matthew Emmett <memmett@gmail.com>
2020-10-22 19:09:19 -0600

    Add Python bluestein version.

---
+++ docs/bluestein/bluestein.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import math
+import numpy as np
+import numpy.linalg as la
+import numpy.random as nr
+from numpy.fft import fft, ifft
+
+def bluestein(x):
+    N = x.size
+    M = 2**int(math.ceil(math.log2(N))+1)
+    assert(2*N <= M)
+
+    n = np.arange(N)
+
+    # A = x * exp() term; A is indexed by n
+    a = np.exp(-1j * np.pi / N * n**2)
+    A = np.zeros(M, x.dtype)
+    A[:N] = x * a
+
+    # B = exp(); B is indexed by k-n
+    b = np.exp(1j * np.pi / N * n**2)
+    B = np.zeros(M, x.dtype)
+    B[0] = b[0]
+    for i in range(1, N):
+        B[i] = b[i]
+        B[M-i] = b[i]
+
+    return a * ifft(fft(A) * fft(B))[:N]
+
+
+def compare(y, f1, f2):
+    y1, y2 = y.copy(), y.copy()
+    k1, k2 = f1(y1), f2(y2)
+    return la.norm(k1-k2) / la.norm(k1)
+
+N = 16
+y = nr.rand(N) + 1j * nr.rand(N)
+
+print('rel error', compare(y, fft, bluestein))
