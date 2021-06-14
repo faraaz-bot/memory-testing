d5ed6284e439dc7f0d28963260484aad3b8c4add
Matthew Emmett <matthew.emmett@amd.com>
2021-06-14 09:26:19 -0600

    Linting.

--- perf/perflib/utils.py
+++ perf/perflib/utils.py
@@ -6,13 +6,10 @@ from typing import List
 
 from functools import reduce
 
-@dataclass
-class Sample:
-    lengths: List[int]
-    nbatch: int
-    times: List[float]
 
-
+#
+# Join shortcuts
+#
 
 def join(sep, s):
     """Return 's' joined with 'sep'.  Coerces to str."""
@@ -39,6 +36,10 @@ def tjoin(s):
     return join('\t', s)
 
 
+#
+# Misc
+#
+
 def shape(n, nbatch):
     """Return NumPy shape."""
     if isinstance(n, (list, tuple)):
@@ -51,38 +52,44 @@ def product(xs):
     return reduce(lambda x, y: x * y, xs, 1)
 
 
+#
+# DAT files
+#
+
+@dataclass
+class Sample:
+    """Dyna-rider/rider timing sample: list of times for a given length+batch."""
+
+    lengths: List[int]
+    nbatch: int
+    times: List[float]
+
+
 def write_dat(fname, length, nbatch, seconds, title=None):
-    """Append record to .dat file."""
+    """Append record to dyna-rider/rider .dat file."""
     path = Path(fname)
     if not path.exists():
         dat = ['# title: ' + title]
     else:
         dat = path.read_text().splitlines()
-
     if isinstance(length, int):
         length = [length]
-
-    record = [ len(length) ] + list(length) + [ nbatch, len(seconds) ] + seconds
+    record = [len(length)] + list(length) + [nbatch, len(seconds)] + seconds
     dat.append(tjoin(record))
-
     path.write_text(njoin(dat) + '\n')
 
 
 def read_dat(fname):
     """Read dyna-rider/rider .dat file."""
-
     dat = Path(fname).read_text()
-
     records = {}
     for line in dat.splitlines():
         if line.startswith('#'):
             continue
         words   = line.split("\t")
         dim     = int(words[0])
-        lengths = tuple(map(int, words[1:dim+1]))
-        nbatch  = int(words[dim+1])
-        times   = list(map(float, words[dim+3:]))
-
+        lengths = tuple(map(int, words[1:dim + 1]))
+        nbatch  = int(words[dim + 1])
+        times   = list(map(float, words[dim + 3:]))
         records[lengths] = Sample(list(lengths), nbatch, times)
-
     return records
