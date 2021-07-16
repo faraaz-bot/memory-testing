b2213fa3c3613c9794188419c164bac290146576
Matthew Emmett <matthew.emmett@amd.com>
2021-07-16 09:56:52 -0600

    Add pointer and variant versions of PoC C++ generator.

---
+++ playground/gen-variant/generator.hpp
@@ -0,0 +1,408 @@
+
+#include <any>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <string>
+#include <variant>
+#include <vector>
+
+//
+// Helpers
+//
+
+template <typename T>
+std::string vrender(const T& x)
+{
+    return std::visit([](const auto a) { return a.render(); }, x);
+}
+
+//
+// Declarations
+//
+
+class Declaration
+{
+public:
+    std::string name;
+    std::string type;
+    Declaration(std::string name, std::string type) : name(name), type(type) {};
+    std::string render() const;
+};
+
+std::string Declaration::render() const {
+    return type + " " + name + ";";
+}
+
+//
+// Expressions
+//
+
+class Variable;
+class Literal;
+
+class Add;
+class Subtract;
+class Multiply;
+class Divide;
+class Modulus;
+
+using Expression = std::variant<Variable, Literal, Add, Subtract, Multiply, Divide, Modulus>;
+
+class Literal
+{
+    std::string value;
+
+public:
+    template <typename T>
+    Literal(T l)
+    {
+        value = std::to_string(l);
+    }
+
+    std::string render() const
+    {
+        return value;
+    }
+};
+
+class Variable
+{
+public:
+    std::string name;
+    // Expression index;
+    std::any index;
+
+    Variable(std::string name)
+        : name(name){};
+
+    Variable operator[](const Expression& index) const;
+    Declaration declaration() const;
+
+    std::string render() const;
+};
+
+#define MAKE_ARITH(NAME, SEP, PRECEDENCE)          \
+    class NAME                                     \
+    {                                              \
+        int const   precedence = PRECEDENCE;       \
+        std::string separator{SEP};                \
+                                                   \
+    public:                                        \
+        std::vector<Expression> args;              \
+        NAME(std::initializer_list<Expression> il) \
+            : args(il){};                          \
+        NAME(std::vector<Expression> il)           \
+            : args(il){};                          \
+        std::string render() const;                \
+    };
+
+#define MAKE_RENDER(NAME)                      \
+    std::string NAME::render() const           \
+    {                                          \
+        std::string s = vrender(args[0]);      \
+        for(int i = 1; i < args.size(); ++i)   \
+            s += separator + vrender(args[i]); \
+        return s;                              \
+    }
+
+MAKE_ARITH(Add, " + ", 10);
+MAKE_ARITH(Multiply, " * ", 20);
+MAKE_ARITH(Subtract, " - ", 15);
+MAKE_ARITH(Divide, " / ", 25);
+MAKE_ARITH(Modulus, " % ", 25);
+
+MAKE_RENDER(Add);
+MAKE_RENDER(Multiply);
+MAKE_RENDER(Subtract);
+MAKE_RENDER(Divide);
+MAKE_RENDER(Modulus);
+
+Declaration Variable::declaration() const {
+    return Declaration(name, "int");
+}
+
+std::string Variable::render() const
+{
+    if(index.has_value())
+    {
+        auto expr = std::any_cast<Expression>(index);
+        return name + "[" + vrender(expr) + "]";
+    }
+    return name;
+}
+
+Variable Variable::operator[](const Expression& index) const
+{
+    auto v  = Variable(name);
+    v.index = index;
+    return v;
+}
+
+Add operator+(const Expression& a, const Expression& b)
+{
+    return Add{a, b};
+}
+
+Subtract operator-(const Expression& a, const Expression& b)
+{
+    return Subtract{a, b};
+}
+
+Multiply operator*(const Expression& a, const Expression& b)
+{
+    return Multiply{a, b};
+}
+
+Divide operator/(const Expression& a, const Expression& b)
+{
+    return Divide{a, b};
+}
+
+Modulus operator%(const Expression& a, const Expression& b)
+{
+    return Modulus{a, b};
+}
+
+//
+// Statements
+//
+
+class Assign;
+class For;
+class StatementList;
+
+using Statement = std::variant<Declaration, Assign, For>;
+//using Statement = std::variant<Assign>;
+
+class Assign
+{
+public:
+    Variable   lhs;
+    Expression rhs;
+
+    Assign(Variable lhs, Expression rhs)
+        : lhs(lhs)
+        , rhs(rhs){};
+
+    std::string render() const
+    {
+        return lhs.render() + " = " + vrender(rhs) + ";";
+    }
+};
+
+class StatementList
+{
+public:
+    std::vector<Statement> statements;
+    StatementList(){};
+    std::string render() const;
+};
+
+class For
+{
+public:
+    Variable      initial;
+    Expression    condition;
+    Expression    iteration;
+    StatementList body;
+    For(Variable initial, Expression condition, Expression iteration)
+        : initial(initial)
+        , condition(condition)
+        , iteration(iteration){};
+    std::string render() const;
+};
+
+std::string StatementList::render() const
+{
+    std::string r;
+    for(auto s : statements)
+        r += vrender(s) + "\n";
+    return r;
+}
+
+void operator+=(StatementList& stmts, const Statement& s)
+{
+    stmts.statements.push_back(s);
+}
+
+std::string For::render() const
+{
+    std::string s;
+    s += "for(";
+    s += initial.render() + "; ";
+    s += vrender(condition) + "; ";
+    s += vrender(iteration) + ") {\n ";
+    s += body.render();
+    s += "\n}\n";
+    return s;
+}
+
+//
+// Functions
+//
+
+class ArgumentList {
+public:
+    ArgumentList() {};
+    ArgumentList(std::vector<Variable> arguments) : arguments(arguments) {};
+    std::vector<Variable> arguments;
+    std::string render() const;
+};
+
+std::string ArgumentList::render() const {
+    std::string f;
+    if (!arguments.empty())
+    {
+        f = arguments[0].render();
+        for (int i = 1; i < arguments.size(); ++i) {
+            f += ",";
+            f += arguments[i].render();
+        }
+    }
+    return f;
+}
+
+
+class Function {
+public:
+    std::string name;
+    StatementList body;
+    ArgumentList arguments;
+
+    Function(std::string name) : name(name) {};
+
+    std::string render() const;
+};
+
+std::string Function::render() const {
+    std::string f;
+    f = "void " + name + "(" + arguments.render() + ") {\n";
+    f += body.render();
+    f += "}\n";
+    return f;
+}
+
+//
+// Example of AST transform
+//
+
+//
+// make_planar
+//
+
+#define MAKE_ARITH_VISITOR(NAME)                  \
+    Expression operator()(const NAME& v)          \
+    {                                             \
+        std::vector<Expression> args;             \
+        for(auto a : v.args)                      \
+        {                                         \
+            args.push_back(std::visit(*this, a)); \
+        }                                         \
+        return Expression{NAME{args}};            \
+    }
+
+struct MakePlanarVisitor
+{
+    std::string old_name{"x"};
+    std::string new_name{"X"};
+
+    Expression operator()(const Variable& x)
+    {
+        auto y = Variable{x};
+        if(x.name == old_name)
+        {
+            y.name = new_name;
+        }
+        if(y.index.has_value())
+        {
+            y.index = std::visit(*this, std::any_cast<Expression>(y.index));
+        }
+        return Expression{y};
+    }
+
+    Expression operator()(const Literal& x)
+    {
+        return Expression{x};
+    }
+
+    MAKE_ARITH_VISITOR(Add)
+    MAKE_ARITH_VISITOR(Subtract)
+    MAKE_ARITH_VISITOR(Multiply)
+    MAKE_ARITH_VISITOR(Divide)
+    MAKE_ARITH_VISITOR(Modulus)
+
+    Statement operator()(const Assign& x)
+    {
+        auto lhs = std::get<Variable>((*this)(x.lhs));
+        auto rhs = std::visit(*this, x.rhs);
+        return Statement{Assign(lhs, rhs)};
+    }
+
+    Statement operator()(const For& x)
+    {
+        auto initial   = std::get<Variable>((*this)(x.initial));
+        auto condition = std::visit(*this, x.condition);
+        auto iteration = std::visit(*this, x.iteration);
+        return Statement{For(initial, condition, iteration)};
+    }
+
+    Statement operator()(const Declaration& x)
+    {
+        return Statement{x};
+    }
+};
+
+StatementList make_planar(const StatementList& stmts)
+{
+    auto visitor = MakePlanarVisitor();
+    auto nstmts  = StatementList();
+    for(auto s : stmts.statements)
+    {
+        nstmts += std::visit(visitor, s);
+    }
+    return nstmts;
+}
+
+
+
+//
+// Test!
+//
+
+template <typename T>
+T product(std::vector<T> x, int last = -1)
+{
+    if(last == 0)
+        return 1;
+    if(last > 0)
+        return std::accumulate(x.cbegin(), x.cbegin() + last, T(1), std::multiplies<T>());
+    return std::accumulate(x.cbegin(), x.cend(), T(1), std::multiplies<T>());
+}
+
+Function make_device_fft(std::vector<int> factors)
+{
+    int length = product(factors);
+    int threads_per_transform = length / factors[0];
+
+    auto kdevice = Function("forward_length" + std::to_string(length));
+
+    auto R = Variable("R");
+    auto thread = Variable("thread");
+    auto thread_id = Variable("threadIdx.x");
+    auto buf = Variable("buf");
+
+    kdevice.body += R.declaration();
+    kdevice.body += thread.declaration();
+    kdevice.body += Assign(thread, thread_id % threads_per_transform);
+
+    for(auto width: factors) {
+        for(int w=0; w < width; ++w) {
+            kdevice.body += Assign(R[w], buf[w]);
+        }
+
+
+    }
+
+    return kdevice;
+}
---
+++ playground/gen-variant/stockham-generator.cpp
@@ -0,0 +1,54 @@
+
+#include "generator.hpp"
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+
+void format_and_write(std::string fname, std::string code)
+{
+    std::ofstream     ofile;
+    std::ifstream     ifile;
+    std::stringstream formatted, existing;
+
+    auto tname = fname + ".tmp";
+
+    ofile.open(tname);
+    ofile << code;
+    ofile.close();
+
+    std::string cmd = "clang-format-10 -i -style=file " + tname;
+    std::system(cmd.c_str());
+    ifile.open(tname);
+    formatted << ifile.rdbuf();
+    ifile.close();
+
+    unlink(tname.c_str());
+
+    bool exists = static_cast<bool>(std::ifstream(fname));
+    if(exists)
+    {
+        ifile.open(fname);
+        existing << ifile.rdbuf();
+        ifile.close();
+
+        if(formatted.str().compare(existing.str()) == 0)
+            return;
+    }
+
+    ofile.open(fname);
+    ofile << formatted.str();
+    ofile.close();
+}
+
+
+int main(int argc, char* argv[])
+{
+    std::vector<int> factors;
+
+    for(int i = 1; i < argc; ++i)
+        factors.push_back(std::stoi(argv[i]));
+
+    auto device = make_device_fft(factors);
+
+    format_and_write("stockham_generated_kernel.h", device.render());
+}
