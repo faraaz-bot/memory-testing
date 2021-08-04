9773aa01c2bba7d3db6ba1428ba6382c05267bf1
Fei.Zheng@amd.com <fei.zheng@amd.com>
2021-08-04 11:14:22 -0600

    split to sync_workgroups_helper.h

--- playground/sync_workgroups/sync_with_atomic.cpp
+++ playground/sync_workgroups/sync_with_atomic.cpp
@@ -9,119 +9,16 @@
 //
 // Build:
 //    with hipcc:
-//      /opt/rocm/bin/hipcc sync_with_atomic.cpp  -o sync_with_atomic
+//      /opt/rocm/bin/hipcc sync_with_atomic.cpp  sync_workgroups_helper.h -o sync_with_atomic
 //    with nvcc:
-//      nvcc -x cu --std=c++11 -D CUDA sync_with_atomic.cpp  -o sync_with_atomic_cuda
+//      nvcc -x cu --std=c++11 -D CUDA sync_with_atomic.cpp  sync_workgroups_helper.h -o sync_with_atomic_cuda
 //
 //
 
-#ifdef CUDA
-#include <cooperative_groups.h>
-#include <cuda_runtime.h>
-#else
-// clang-format off
-#include <hip/hip_runtime.h>
-#include <hip/hip_cooperative_groups.h>
-// clang-format on
-#endif
-
-#include <iostream>
-
-using namespace cooperative_groups;
-using cooperative_groups::thread_group;
-namespace cg = cooperative_groups;
+#include "sync_workgroups_helper.h"
 
 #define LEN 4
 #define BATCH 16
-
-//-----------------------------------------------------------------------------
-// Helper functions
-
-#define GPU_ERR_CHECK(expr)                     \
-    {                                           \
-        gpu_assert((expr), __FILE__, __LINE__); \
-    }
-
-#ifdef CUDA
-inline void gpu_assert(cudaError_t e, const char* file, int line, bool abort = true)
-{
-    if(e != cudaSuccess)
-    {
-        const char* errName = cudaGetErrorName(e);
-        const char* errMsg  = cudaGetErrorString(e);
-        std::cerr << "Error " << e << "(" << errName << ") " << errMsg << std::endl;
-        exit(e);
-    }
-}
-#else
-inline void gpu_assert(hipError_t e, const char* file, int line, bool abort = true)
-{
-    if(e)
-    {
-        const char* errName = hipGetErrorName(e);
-        const char* errMsg  = hipGetErrorString(e);
-        std::cerr << "Error " << e << "(" << errName << ") " << __FILE__ << ":" << __LINE__ << ": "
-                  << std::endl
-                  << errMsg << std::endl;
-        exit(e);
-    }
-}
-#endif
-
-inline void device_synchronize()
-{
-#ifdef CUDA
-    GPU_ERR_CHECK(cudaDeviceSynchronize());
-#else
-    GPU_ERR_CHECK(hipDeviceSynchronize());
-#endif
-}
-
-inline void device_malloc(void** ptr, size_t bytes)
-{
-#ifdef CUDA
-    GPU_ERR_CHECK(cudaMalloc(ptr, bytes));
-#else
-    GPU_ERR_CHECK(hipMalloc(ptr, bytes));
-#endif
-}
-
-inline void device_free(void* ptr)
-{
-#ifdef CUDA
-    cudaFree(ptr);
-#else
-    hipFree(ptr);
-#endif
-}
-
-inline void device_memcpy_h2d(void* dst, const void* src, size_t bytes)
-{
-#ifdef CUDA
-    cudaMemcpy(dst, src, bytes, cudaMemcpyHostToDevice);
-#else
-    hipMemcpy(dst, src, bytes, hipMemcpyHostToDevice);
-#endif
-}
-
-inline void device_memcpy_d2h(void* dst, const void* src, size_t bytes)
-{
-#ifdef CUDA
-    cudaMemcpy(dst, src, bytes, cudaMemcpyDeviceToHost);
-#else
-    hipMemcpy(dst, src, bytes, hipMemcpyDeviceToHost);
-#endif
-}
-
-inline void
-    coop_launch(const void* func, dim3 gridDim, dim3 blockDim, void** args, size_t sharedMem)
-{
-#ifdef CUDA
-    GPU_ERR_CHECK(cudaLaunchCooperativeKernel(func, gridDim, blockDim, args, sharedMem, 0));
-#else
-    GPU_ERR_CHECK(hipLaunchCooperativeKernel(func, gridDim, blockDim, args, sharedMem, 0));
-#endif
-}
 
 //-----------------------------------------------------------------------------
 // Device functions
