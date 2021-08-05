3a44f0c341c0a7730101913f656b99b7ad4ffa34
Fei.Zheng@amd.com <fei.zheng@amd.com>
2021-08-04 23:27:40 -0600

    sync_with_atomic.cpp: comment out bad solution

--- playground/sync_workgroups/sync_with_atomic.cpp
+++ playground/sync_workgroups/sync_with_atomic.cpp
@@ -5,7 +5,9 @@
 // launch as an experiment.
 //
 // The device kernels do simple "plus one" twice on an int array[BATCH * LEN]
-// with grid(BATCH), blocks(LEN).
+// with grid(BATCH), blocks(LEN). Each thread does "plus one" inplace in the 1st
+// round and does "plus one" with LEN right shift in the 2nd round, which requires
+// memfence between them.
 //
 // Build:
 //    with hipcc:
@@ -25,23 +27,23 @@
 
 __device__ int g_counter = 0;
 
-void __device__ plus_one_device(int* a)
+void __device__ plus_one_device(int* a, int offset)
 {
     __shared__ int lds[LEN];
-    lds[threadIdx.x] = a[blockIdx.x * blockDim.x + threadIdx.x];
+    lds[threadIdx.x] = a[offset + threadIdx.x];
 
     __syncthreads();
 
-    lds[threadIdx.x]                         = lds[threadIdx.x] + 1;
-    a[blockIdx.x * blockDim.x + threadIdx.x] = lds[threadIdx.x];
+    lds[threadIdx.x]        = lds[threadIdx.x] + 1;
+    a[offset + threadIdx.x] = lds[threadIdx.x];
 }
 
 void __global__ plus_one_twice_atomic_atomicOr(int* a)
 {
     // do the 1st round task
-    plus_one_device(a);
+    plus_one_device(a, blockIdx.x % BATCH * LEN);
 
-    //__syncthreads();
+    __syncthreads();
 
     // the leading thread in each workgroup sets its bit in g_counter,
     // and then wait for all workgroups sync.
@@ -53,10 +55,11 @@ void __global__ plus_one_twice_atomic_atomicOr(int* a)
         {
         }
     }
+
     __threadfence();
 
     // do the 2nd round task
-    plus_one_device(a);
+    plus_one_device(a, (blockIdx.x + 1) % BATCH * LEN);
 
     // clean up g_counter with leading thread in the 1st workgroup only
     if(threadIdx.x == 0 && blockIdx.x == 0)
@@ -65,28 +68,27 @@ void __global__ plus_one_twice_atomic_atomicOr(int* a)
 
 // Bad example with race condition!!!
 // Do the same as plus_one_twice_atomic_atomicOr but with __threadfence wait
-void __global__ plus_one_twice_atomic_memfence(int* a)
-{
-    plus_one_device(a);
-    //__syncthreads();
+// void __global__ plus_one_twice_atomic_memfence(int* a)
+// {
+//     plus_one_device(a, blockIdx.x % BATCH * LEN);
 
-    if(threadIdx.x == 0)
-    {
-        atomicOr(&g_counter, 0x1 << blockIdx.x);
+//     if(threadIdx.x == 0)
+//     {
+//         atomicOr(&g_counter, 0x1 << blockIdx.x);
 
-        __threadfence();
-        while(g_counter != 0xFFFF)
-        {
-        }
-    }
+//         __threadfence();
+//         while(g_counter != 0xFFFF)
+//         {
+//         }
+//     }
 
-    __threadfence();
+//     __threadfence();
 
-    plus_one_device(a);
+//     plus_one_device(a, (blockIdx.x + 1) % BATCH * LEN);
 
-    if(threadIdx.x == 0 && blockIdx.x == 0)
-        atomicExch(&g_counter, 0);
-}
+//     if(threadIdx.x == 0 && blockIdx.x == 0)
+//         atomicExch(&g_counter, 0);
+// }
 
 //-----------------------------------------------------------------------------
 
@@ -99,7 +101,7 @@ int main()
     int* h_out = new int[total_size];
     int* d_data;
 
-    int trial = 100;
+    int trial = 1000;
 
     for(auto i = 0; i < total_size; i++)
         h_in[i] = i;
@@ -123,29 +125,30 @@ int main()
     }
     device_synchronize();
 
-    std::cout << "------ 2 normal launch: plus_one_twice_atomic_memfence\n";
-    for(auto j = 0; j < trial; j++)
-    {
-        plus_one_twice_atomic_memfence<<<BATCH, LEN>>>(d_data);
-    }
-    device_synchronize();
+    // Bad example with race condition!!!
+    // std::cout << "------ 2 normal launch: plus_one_twice_atomic_memfence\n";
+    // for(auto j = 0; j < trial; j++)
+    // {
+    //     plus_one_twice_atomic_memfence<<<BATCH, LEN>>>(d_data);
+    // }
+    // device_synchronize();
 
-    std::cout << "------ 3 cooper launch: plus_one_twice_atomic_memfence\n";
-    for(auto j = 0; j < trial; j++)
-    {
-        coop_launch((void*)plus_one_twice_atomic_atomicOr, BATCH, LEN, kernelArgs, 0);
-    }
-    device_synchronize();
+    // std::cout << "------ 3 cooper launch: plus_one_twice_atomic_memfence\n";
+    // for(auto j = 0; j < trial; j++)
+    // {
+    //     coop_launch((void*)plus_one_twice_atomic_atomicOr, BATCH, LEN, kernelArgs, 0);
+    // }
+    // device_synchronize();
 
     device_memcpy_d2h(h_out, d_data, total_bytes);
     device_free(d_data);
 
-    const int KERNEL_RUNS = 4;
+    const int KERNEL_RUNS = 2;
     for(auto i = 0; i < total_size; i++)
     {
         if(h_out[i] != (i + KERNEL_RUNS * trial * 2)) //plus one twice for each run
         {
-            std::cout << "failed at " << i << ",  " << h_out[i] << " should be "
+            std::cout << "Failed at " << i << ",  " << h_out[i] << " should be "
                       << (i + KERNEL_RUNS * trial * 2) << std::endl;
             exit(-1);
         }
