349d69ec69bc0d2cfd79ad5555031b83dd696209
Fei.Zheng@amd.com <fei.zheng@amd.com>
2021-09-29 15:15:13 -0600

    sync_workgroups: better error checking

--- playground/sync_workgroups/sync_workgroups_helper.h
+++ playground/sync_workgroups/sync_workgroups_helper.h
@@ -24,36 +24,39 @@ namespace cg = cooperative_groups;
 //-----------------------------------------------------------------------------
 // Error check
 
-#define GPU_ERR_CHECK(expr)                     \
-    {                                           \
-        gpu_assert((expr), __FILE__, __LINE__); \
-    }
-
+inline const char* gpu_get_err_name(int e)
+{
 #ifdef CUDA
-inline void gpu_assert(cudaError_t e, const char* file, int line, bool abort = true)
-{
-    if(e != cudaSuccess)
-    {
-        const char* errName = cudaGetErrorName(e);
-        const char* errMsg  = cudaGetErrorString(e);
-        std::cerr << "Error " << e << "(" << errName << ") " << errMsg << std::endl;
-        exit(e);
-    }
-}
+    return cudaGetErrorName((cudaError_t)e);
 #else
-inline void gpu_assert(hipError_t e, const char* file, int line, bool abort = true)
-{
-    if(e)
-    {
-        const char* errName = hipGetErrorName(e);
-        const char* errMsg  = hipGetErrorString(e);
-        std::cerr << "Error " << e << "(" << errName << ") " << __FILE__ << ":" << __LINE__ << ": "
-                  << std::endl
-                  << errMsg << std::endl;
-        exit(e);
-    }
-}
+    return hipGetErrorName((hipError_t)e);
 #endif
+}
+
+inline const char* gpu_get_err_string(int e)
+{
+#ifdef CUDA
+    return cudaGetErrorString((cudaError_t)e);
+#else
+    return hipGetErrorString((hipError_t)e);
+#endif
+}
+
+#define GPU_ERR_CHECK(call)                                                                    \
+    if(1)                                                                                      \
+    {                                                                                          \
+        auto ec = (call);                                                                      \
+        if(ec)                                                                                 \
+        {                                                                                      \
+            const char* errName;                                                               \
+            const char* errMsg;                                                                \
+            std::cerr << "Failed at " << __FILE__ << ":" << __LINE__ << " in " << __FUNCTION__ \
+                      << "()\n"                                                                \
+                      << "Error code: " << ec << ", " << gpu_get_err_name(ec) << ", "          \
+                      << gpu_get_err_string(ec) << std::endl;                                  \
+            exit(ec);                                                                          \
+        }                                                                                      \
+    }
 
 //-----------------------------------------------------------------------------
 // API wrappers
@@ -88,27 +91,36 @@ inline void device_malloc(void** ptr, size_t bytes)
 inline void device_free(void* ptr)
 {
 #ifdef CUDA
-    cudaFree(ptr);
+    GPU_ERR_CHECK(cudaFree(ptr));
 #else
-    hipFree(ptr);
+    GPU_ERR_CHECK(hipFree(ptr));
 #endif
 }
 
 inline void device_memcpy_h2d(void* dst, const void* src, size_t bytes)
 {
 #ifdef CUDA
-    cudaMemcpy(dst, src, bytes, cudaMemcpyHostToDevice);
+    GPU_ERR_CHECK(cudaMemcpy(dst, src, bytes, cudaMemcpyHostToDevice));
 #else
-    hipMemcpy(dst, src, bytes, hipMemcpyHostToDevice);
+    GPU_ERR_CHECK(hipMemcpy(dst, src, bytes, hipMemcpyHostToDevice));
 #endif
 }
 
 inline void device_memcpy_d2h(void* dst, const void* src, size_t bytes)
 {
 #ifdef CUDA
-    cudaMemcpy(dst, src, bytes, cudaMemcpyDeviceToHost);
+    GPU_ERR_CHECK(cudaMemcpy(dst, src, bytes, cudaMemcpyDeviceToHost));
 #else
-    hipMemcpy(dst, src, bytes, hipMemcpyDeviceToHost);
+    GPU_ERR_CHECK(hipMemcpy(dst, src, bytes, hipMemcpyDeviceToHost));
+#endif
+}
+
+inline void device_memcpy_to_symbol_h2d(const void* dst, const void* src, size_t bytes)
+{
+#ifdef CUDA
+    GPU_ERR_CHECK(cudaMemcpyToSymbol(dst, src, bytes));
+#else
+    GPU_ERR_CHECK(hipMemcpyToSymbol(HIP_SYMBOL(dst), src, bytes));
 #endif
 }
 
