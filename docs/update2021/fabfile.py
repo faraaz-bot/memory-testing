e7380cf5edc464cd7ba8db465f142be1fc620931
Matthew Emmett <matthew.emmett@amd.com>
2021-12-14 09:06:17 -0700

    Updates to update2021.

--- docs/update2021/fabfile.py
+++ docs/update2021/fabfile.py
@@ -22,50 +22,68 @@ To follow along, add '--echo' to any of the above.
 
 from fabric import task
 
-import collections
+import dataclasses
 import os
 import pathlib
 import subprocess
+
+
+@dataclasses.dataclass
+class Run:
+    tag: str
+    device: str
+    rider: str
+    suite: str
+
+
+@dataclasses.dataclass
+class Repo:
+    url: str
+    commit: str
+
+    @property
+    def name(self):
+        name = self.url.split(':')[1]
+        name = name.split('/')[1]
+        return name.split('-')[0]
+
+    @property
+    def path(self):
+        return pathlib.Path(self.name + '-' + self.commit)
+
 
 #
 # configuration
 #
 
+user       = os.getenv('USER')
 comparison = 'update2021'
-suite_file = 'rocFFT-misc/docs/update2021/s21.py'
+suite_file = 'docs/update2021/s21.py'
 
-rocfft_repo        = 'git@github.com:memmett/rocFFT-internal.git'
-rocfft_commit      = 'cholla1d-optimize'
-# rocfft_repo        = 'git@github.com:ROCmSoftwarePlatform/rocFFT-internal.git'
-# rocfft_commit      = 'develop'
-hipfft_repo        = 'git@github.com:ROCmSoftwarePlatform/hipFFT-internal.git'
-hipfft_commit      = 'develop'
-rocfft_perf_repo   = 'git@github.com:ROCmSoftwarePlatform/rocFFT-internal.git'
-rocfft_misc_repo   = 'git@github.com:ROCmSoftwarePlatform/rocFFT-misc.git'
-
-Run = collections.namedtuple('Run', ['tag', 'device', 'rider', 'suite'])
-
-user = os.getenv('USER')
+rocfft  = Repo('git@github.com:memmett/rocFFT-internal.git', '122f348')
+hipfft  = Repo('git@github.com:ROCmSoftwarePlatform/hipFFT-internal.git', '9b150b0')
+fftperf = Repo('git@github.com:memmett/rocFFT-internal.git', '122f348')
+#fftperf = Repo('git@github.com:ROCmSoftwarePlatform/rocFFT-internal.git', '0a6f539')
+fftmisc = Repo('git@github.com:ROCmSoftwarePlatform/rocFFT-misc.git', 'f055e4a')
 
 runs = [
-    Run('rocfft-mi100-md', 'mi100', 'rocfft', 's21:md1'),
+    Run('rocfft-mi100-md',     'mi100', 'rocfft', 's21:md1'),
     Run('rocfft-mi100-cholla', 'mi100', 'rocfft', 's21:cholla1d'),
-    Run('rocfft-mi200-md', 'mi200', 'rocfft', 's21:md1'),
+    Run('rocfft-mi200-md',     'mi200', 'rocfft', 's21:md1'),
     Run('rocfft-mi200-cholla', 'mi200', 'rocfft', 's21:cholla1d'),
-    Run('cufft-v100-md', 'v100', 'hipfft', 's21:md1'),
-    Run('cufft-v100-cholla', 'v100', 'hipfft', 's21:cholla1d'),
-    Run('cufft-a100-md', 'a100', 'hipfft', 's21:md1'),
-    Run('cufft-a100-cholla', 'a100', 'hipfft', 's21:cholla1d'),
+    Run('cufft-v100-md',        'v100', 'hipfft', 's21:md1'),
+    Run('cufft-v100-cholla',    'v100', 'hipfft', 's21:cholla1d'),
+    Run('cufft-a100-md',        'a100', 'hipfft', 's21:md1'),
+    Run('cufft-a100-cholla',    'a100', 'hipfft', 's21:cholla1d'),
 ]
 
 #
 # shortcuts
 #
 
-rocfft_perf  = pathlib.Path('rocFFT-internal') / 'scripts' / 'perf' / 'rocfft-perf'
-hipfft_perf  = pathlib.Path('rocFFT-internal') / 'scripts' / 'perf' / 'rocfft-perf'
-rocfft_rider = pathlib.Path(f'rocFFT-{rocfft_commit}') / 'build' / 'clients' / 'staging' / 'rocfft-rider'
-hipfft_rider = pathlib.Path(f'hipFFT-{hipfft_commit}') / 'build' / 'clients' / 'staging' / 'hipfft-rider'
+rocfft_perf  = fftperf.path / 'scripts' / 'perf' / 'rocfft-perf'
+rocfft_rider = rocfft.path / 'build' / 'clients' / 'staging' / 'rocfft-rider'
+hipfft_rider = hipfft.path / 'build' / 'clients' / 'staging' / 'hipfft-rider'
 
 #
 # helpers
@@ -82,14 +100,8 @@ def docker(command, workdir='/x'):
 
 
 def pull(c, dname):
-    """Pull directory 'dname' from remote host."""
-    s = c.sftp()
-    s.chdir(comparison)
-    d = pathlib.Path(dname)
-    d.mkdir(exist_ok=True)
-    for f in s.listdir(dname):
-        fname = str(d / f)
-        s.get(fname, fname)
+    """Sync directory 'dname' from remote host."""
+    subprocess.run(['rsync', '-a', c.host + ':' + comparison + '/' + dname + '/', dname + '/'])
 
 
 #
@@ -104,19 +116,13 @@ def setup(c):
         c.run(f'mkdir {comparison}')
 
     with c.cd(comparison):
-        if not remote_exists(c, 'rocFFT-internal'):
-            c.run(f'git clone {rocfft_perf_repo}')
-        else:
-            with c.cd('rocFFT-internal'):
-                c.run('git pull')
+        for repo in [fftperf, fftmisc]:
+            if not remote_exists(c, repo.path):
+                c.run(f'git clone {repo.url} {repo.path}')
+            with c.cd(repo.path):
+                c.run(f'git checkout {repo.commit}')
 
-        if not remote_exists(c, 'rocFFT-misc'):
-            c.run(f'git clone {rocfft_misc_repo}')
-        else:
-            with c.cd('rocFFT-misc'):
-                c.run('git pull')
-
-        spath = pathlib.Path(suite_file)
+        spath = fftmisc.path / suite_file
         sname = spath.name
         c.run(f'cp {spath} {sname}')
 
@@ -125,15 +131,14 @@ def setup(c):
 def build_rocfft(c):
     """Build rocFFT rider (native)."""
     with c.cd(comparison):
-        if not remote_exists(c, f'rocFFT-{rocfft_commit}'):
-            c.run(f'git clone {rocfft_repo} rocFFT-{rocfft_commit}')
+        if not remote_exists(c, rocfft.path):
+            c.run(f'git clone {rocfft.url} {rocfft.path}')
 
-        with c.cd(f'rocFFT-{rocfft_commit}'):
-            c.run(f'git checkout {rocfft_commit}')
-            c.run('git pull')
+        with c.cd(rocfft.path):
+            c.run(f'git checkout {rocfft.commit}')
             c.run('mkdir -p build')
             with c.cd('build'):
-                sqlite = '-DSQLITE_SRC_URL=file:///home/maemmett/sqlite-amalgamation-3360000.zip'
+                sqlite = '-DSQLITE_SRC_URL=file:///home/maemmett/sqlite-amalgamation-3360000.zip'  # XXX
                 c.run(f'cmake -G Ninja -DCMAKE_CXX_COMPILER=hipcc -DBUILD_CLIENTS_RIDER=ON -DAMDGPU_TARGETS= {sqlite} ..')
                 c.run('ninja')
 
@@ -143,10 +148,10 @@ def run_rocfft(c, device):
     """Run rocFFT runs."""
     rruns = [x for x in runs if x.rider == 'rocfft' and x.device == device]
     with c.cd(comparison):
-        for tag, _, rider, suite in rruns:
-            c.run(f'rm -rf {tag}')
-            c.run(f'{rocfft_perf} run --rider {rocfft_rider} --suite {suite} --out {tag}')
-            pull(c, tag)
+        for run in rruns:
+            c.run(f'rm -rf {run.tag}')
+            c.run(f'{rocfft_perf} run --rider {rocfft_rider} --suite {run.suite} --out {run.tag}')
+            pull(c, run.tag)
 
 
 @task
@@ -160,15 +165,14 @@ def build_docker(c):
 def build_hipfft(c):
     """Build hipFFT rider (docker)."""
     with c.cd(comparison):
-        if not remote_exists(c, f'hipFFT-{hipfft_commit}'):
-            c.run(f'git clone {hipfft_repo} hipFFT-{hipfft_commit}')
+        if not remote_exists(c, hipfft.path):
+            c.run(f'git clone {hipfft.url} {hipfft.path}')
 
-        with c.cd(f'hipFFT-{hipfft_commit}'):
-            c.run(f'git checkout {hipfft_commit}')
-            c.run('git pull')
+        with c.cd(hipfft.path):
+            c.run(f'git checkout {hipfft.commit}')
             c.run('mkdir -p build')
 
-            build = f'/x/{comparison}/hipFFT-{hipfft_commit}/build'
+            build = f'/x/{comparison}/{hipfft.path}/build'
             c.run(docker('cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DBUILD_CLIENTS=ON -DBUILD_WITH_LIB=CUDA ..', workdir=build))
             c.run(docker('make', workdir=build))
 
@@ -178,8 +182,8 @@ def run_hipfft(c, device):
     """Run hipFFT runs."""
     rruns = [x for x in runs if x.rider == 'hipfft' and x.device == device]
     with c.cd(comparison):
-        for tag, _, rider, suite in rruns:
+        for run in rruns:
             work = f'/x/{comparison}'
-            c.run(docker(f'rm -rf {tag}', workdir=work))
-            c.run(docker(f'{hipfft_perf} run --rider {hipfft_rider} --suite {suite} --out {tag}', workdir=work))
-            pull(c, tag)
+            c.run(docker(f'rm -rf {run.tag}', workdir=work))
+            c.run(docker(f'{rocfft_perf} run --rider {hipfft_rider} --suite {run.suite} --out {run.tag}', workdir=work))
+            pull(c, run.tag)
