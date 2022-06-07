9937f1ca832c0f5d7301a9a60001ac187f8cc731
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2022-06-07 09:45:34 -0600

    Output token in hipbw-rider so that it works with the tokenized rocfft-perf script.

--- riders/hipbw/hipbw-rider.cpp
+++ riders/hipbw/hipbw-rider.cpp
@@ -212,6 +212,8 @@ int main(int argc, char* argv[])
     // test parameters:
     hipbw_params params;
 
+    // Token string to fully specify fft params.
+    std::string token;
     // Declare the supported options.
 
     // clang-format doesn't handle boost program options very well:
@@ -250,7 +252,8 @@ int main(int argc, char* argv[])
         ("osize", po::value<std::vector<size_t>>(&params.osize)->multitoken(),
          "Logical size of output buffer.")
         ("ioffset", po::value<std::vector<size_t>>(&params.ioffset)->multitoken(), "Input offsets.")
-        ("ooffset", po::value<std::vector<size_t>>(&params.ooffset)->multitoken(), "Output offsets.");
+        ("ooffset", po::value<std::vector<size_t>>(&params.ooffset)->multitoken(), "Output offsets.")
+        ("token", po::value<std::string>(&token));
     //clang-format on
 
     po::variables_map vm;
@@ -260,26 +263,7 @@ int main(int argc, char* argv[])
     if(vm.count("help"))
     {
         std::cout << opdesc << std::endl;
-        return 0;
-    }
-
-    if(!vm.count("length"))
-    {
-        std::cout << "Please specify transform length!" << std::endl;
-        std::cout << opdesc << std::endl;
-        return 0;
-    }
-
-    params.placement = vm.count("notInPlace") ? fft_placement_notinplace : fft_placement_inplace;
-    params.precision = vm.count("double") ? fft_precision_double : fft_precision_single;
-
-    if(vm.count("notInPlace"))
-    {
-        std::cout << "out-of-place\n";
-    }
-    else
-    {
-        std::cout << "in-place\n";
+        return EXIT_SUCCESS;
     }
 
     if(vm.count("ntrial"))
@@ -287,51 +271,88 @@ int main(int argc, char* argv[])
         std::cout << "Running profile with " << ntrial << " samples\n";
     }
 
-    if(vm.count("length"))
+    if(token != "")
     {
-        std::cout << "length:";
-        for(auto& i : params.length)
-            std::cout << " " << i;
-        std::cout << "\n";
-    }
+        std::cout << "Reading fft params from token:\n" << token << std::endl;
 
-    if(vm.count("istride"))
-    {
-        std::cout << "istride:";
-        for(auto& i : params.istride)
-            std::cout << " " << i;
-        std::cout << "\n";
+        try
+        {
+            params.from_token(token);
+        }
+        catch(...)
+        {
+            std::cout << "Unable to parse token." << std::endl;
+            return 1;
+        }
     }
-    if(vm.count("ostride"))
+    else
     {
-        std::cout << "ostride:";
-        for(auto& i : params.ostride)
-            std::cout << " " << i;
-        std::cout << "\n";
-    }
+        if(!vm.count("length"))
+        {
+            std::cout << "Please specify transform length!" << std::endl;
+            std::cout << opdesc << std::endl;
+            return EXIT_SUCCESS;
+        }
 
-    if(params.idist > 0)
-    {
-        std::cout << "idist: " << params.idist << "\n";
-    }
-    if(params.odist > 0)
-    {
-        std::cout << "odist: " << params.odist << "\n";
-    }
+        params.placement
+            = vm.count("notInPlace") ? fft_placement_notinplace : fft_placement_inplace;
+        params.precision = vm.count("double") ? fft_precision_double : fft_precision_single;
 
-    if(vm.count("ioffset"))
-    {
-        std::cout << "ioffset:";
-        for(auto& i : params.ioffset)
-            std::cout << " " << i;
-        std::cout << "\n";
-    }
-    if(vm.count("ooffset"))
-    {
-        std::cout << "ooffset:";
-        for(auto& i : params.ooffset)
-            std::cout << " " << i;
-        std::cout << "\n";
+        if(vm.count("notInPlace"))
+        {
+            std::cout << "out-of-place\n";
+        }
+        else
+        {
+            std::cout << "in-place\n";
+        }
+
+        if(vm.count("length"))
+        {
+            std::cout << "length:";
+            for(auto& i : params.length)
+                std::cout << " " << i;
+            std::cout << "\n";
+        }
+
+        if(vm.count("istride"))
+        {
+            std::cout << "istride:";
+            for(auto& i : params.istride)
+                std::cout << " " << i;
+            std::cout << "\n";
+        }
+        if(vm.count("ostride"))
+        {
+            std::cout << "ostride:";
+            for(auto& i : params.ostride)
+                std::cout << " " << i;
+            std::cout << "\n";
+        }
+
+        if(params.idist > 0)
+        {
+            std::cout << "idist: " << params.idist << "\n";
+        }
+        if(params.odist > 0)
+        {
+            std::cout << "odist: " << params.odist << "\n";
+        }
+
+        if(vm.count("ioffset"))
+        {
+            std::cout << "ioffset:";
+            for(auto& i : params.ioffset)
+                std::cout << " " << i;
+            std::cout << "\n";
+        }
+        if(vm.count("ooffset"))
+        {
+            std::cout << "ooffset:";
+            for(auto& i : params.ooffset)
+                std::cout << " " << i;
+            std::cout << "\n";
+        }
     }
 
     std::cout << std::flush;
@@ -347,6 +368,13 @@ int main(int argc, char* argv[])
         throw std::runtime_error("Invalid parameters, add --verbose=1 for detail");
     }
 
+
+    std::cout << "Token: " << params.token() << std::endl;
+    if(verbose)
+    {
+        std::cout << params.str(" ") << std::endl;
+    }
+    
     if(verbose)
     {
         std::cout << params.str() << std::endl;
