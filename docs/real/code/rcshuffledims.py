2d532a75742d4c804bdd45d756f01c85c27f1298
Cheyenne Goh <cheyenne.goh@amd.com>
2023-05-10 14:35:21 -0600

    Add shuffling logic

--- docs/real/code/rcshuffledims.py
+++ docs/real/code/rcshuffledims.py
@@ -5,9 +5,23 @@ import random
 
 np.set_printoptions(precision=3)
 
+def array_random_strides(a):
+    axes = []
+    for i in range(len(a)):
+        axes.append(i)
+    
+    axes = np.random.permutation(axes)
+
+    dims = []
+    for axis in axes:
+        dims.append(a[axis])
+    f = np.empty(dims)
+
+    return np.transpose(f, np.argsort(axes))
+
 def rinit_2d(nx, ny, seed=10):
     random.seed(seed)
-    f = np.empty([nx, ny])
+    f = array_random_strides([nx, ny])
     for i in range(nx):
         for j in range(ny):
             f[i, j] = random.random()
@@ -15,7 +29,7 @@ def rinit_2d(nx, ny, seed=10):
 
 def rinit_3d(nx, ny, nz, seed=10):
     random.seed(seed)
-    f = np.empty([nx, ny, nz])
+    f = array_random_strides([nx, ny, nz])
     for i in range(nx):
         for j in range(ny):
             for k in range(nz):
@@ -26,7 +40,7 @@ def impose_expected_format_2d(tdata, nx, ny):
     sdata = np.empty([nx, ny // 2 + 1], dtype=complex)
 
     for j in range(ny // 2 + 1):
-        # fill up to nx // 2 + 1 with copied values
+        # fill section with copied values
         for i in range(nx // 2 + 1):
             sdata[i][j] = tdata[i][j]
         
@@ -36,18 +50,36 @@ def impose_expected_format_2d(tdata, nx, ny):
 
     return sdata
 
-def impose_expected_format_3d(tdata, nx, ny, nz):
-    sdata = np.zeros([nx, ny, nz // 2 + 1], dtype=complex)
+def impose_expected_format_3d(tdata, nx, ny, nz, axes):
+    sdata = np.empty([nx, ny, nz // 2 + 1], dtype=complex)
 
     for k in range(nz // 2 + 1):
-        for j in range(ny):
-            # fill up to nx // 2 + 1 with copied values
+        for j in range(ny // 2 + 1):
+            # fill section with copied values
             for i in range(nx // 2 + 1):
                 sdata[i][j][k] = tdata[i][j][k]
-
-        # fill remaining with conjugate values
+            
+            # fill section with copied or conjugate values depending on transpose axes
             for i in range(nx // 2 + 1, nx):
-                sdata[i][j][k] = tdata[nx - i][0 if j == 0 else ny - j][0 if k == 0 else nz - k].conj()
+                if axes[2] == 0:
+                    sdata[i][j][k] = tdata[nx - i][0 if j == 0 else ny - j][0 if k == 0 else nz - k].conj()
+                else:
+                    sdata[i][j][k] = tdata[i][j][k]
+    
+        for j in range(ny // 2 + 1, ny):
+            # fill section with copied or conjugate values depending on transpose axes
+            for i in range(nx // 2 + 1):
+                if axes[2] == 1:
+                    sdata[i][j][k] = tdata[0 if i ==  0 else nx - i][ny - j][0 if k == 0 else nz - k].conj()
+                else:
+                    sdata[i][j][k] = tdata[i][j][k]
+            
+            # fill section with copied or conjugate values depending on transpose axes
+            for i in range(nx // 2 + 1, nx):
+                if axes[2] == 2:
+                    sdata[i][j][k] = tdata[i][j][k]
+                else:
+                    sdata[i][j][k] = tdata[0 if i ==  0 else nx - i][ny - j][0 if k == 0 else nz - k].conj()
 
     return sdata
 
@@ -55,7 +87,12 @@ def test_2d(nx, ny, seed):
     # initialize data
     x = rinit_2d(nx, ny, seed)
 
-    # transpose direction
+    # no need to transpose
+    if (not nx % 2 == 0 and ny % 2 == 0
+        or x.strides[0] > x.strides[1] and (not nx % 2 == 0 or ny % 2 == 0)):
+        return
+    
+    # transpose directions
     x_transpose = np.transpose(x)
 
     # execute real-complex transform
@@ -71,7 +108,7 @@ def test_2d(nx, ny, seed):
     X_oracle = np.fft.rfft2(x)
 
     # compare result to oracle
-    if (not np.allclose(X_oracle, X_format)):
+    if not np.allclose(X_oracle, X_format):
         print("Failed", nx, ny, seed)
         exit(0)
 
@@ -79,34 +116,64 @@ def test_3d(nx, ny, nz, seed):
     # initialize data
     x = rinit_3d(nx, ny, nz, seed)
 
-    # transpose direction
-    x_transpose = np.transpose(x)
+    dims = [nx, ny, nz]
+    axes = [0, 1, 2]
 
+    # sort strides in descending order
+    if x.strides[axes[1]] < x.strides[axes[2]]:
+        axes[1], axes[2] = axes[2], axes[1]
+    if x.strides[axes[0]] < x.strides[axes[2]]:
+        axes[0], axes[2] = axes[2], axes[0]
+    if x.strides[axes[0]] < x.strides[axes[1]]:
+        axes[0], axes[1] = axes[1], axes[0]
+
+
+    # maximize contiguity for even dimensions
+    if (dims[axes[0]] % 2 != 0):
+        if (dims[axes[1]] % 2 == 0) and (dims[axes[2]] % 2 != 0):
+            axes[1], axes[2] = axes[2], axes[1]
+    else:
+        if (dims[axes[1]] % 2 == 0):
+            if (dims[axes[2]] % 2 != 0):
+                axes = np.roll(axes, 1)
+        else:
+            if (dims[axes[2]] % 2 == 0):
+                axes[0], axes[1] = axes[1], axes[0]
+            else:
+                axes = np.roll(axes, -1)
+
+    # no need to transpose
+    if list(axes) == [0, 1, 2]:
+        return
+ 
+    # transpose direction
+    x_transpose = np.transpose(x, axes)
+    
     # execute real-complex transform
     X_transpose = np.fft.rfftn(x_transpose)
 
     # transpose back
-    X = np.transpose(X_transpose)
+    X = np.transpose(X_transpose, np.argsort(axes))
 
     # impose expected format
-    X_format = impose_expected_format_3d(X, nx, ny, nz)
+    X_format = impose_expected_format_3d(X, nx, ny, nz, axes)
 
     # compute oracle
     X_oracle = np.fft.rfftn(x)
 
-    if (not np.allclose(X_oracle, X_format)):
+    if not np.allclose(X_oracle, X_format):
         print("Failed", nx, ny, nz, seed)
         exit(0)
 
 # run 2d tests
-# for nx in range(1, 100 + 1):
-#     for ny in range(1, 100 + 1):
-#         for seed in range(3):
-#             test_2d(nx, ny, seed)
+for nx in range(1, 100 + 1):
+    for ny in range(1, 100 + 1):
+        for seed in range(3):
+            test_2d(nx, ny, seed)
 
 # run 3d tests
-# for nx in range(1, 25 + 1):
-#     for ny in range(1, 25 + 1):
-#         for nz in range (1, 25 + 1):
-#             for seed in range(3):
-#                 test_3d(nx, ny, nz, seed)
+for nx in range(1, 25 + 1):
+    for ny in range(1, 25 + 1):
+        for nz in range (1, 25 + 1):
+            for seed in range(3):
+                test_3d(nx, ny, nz, seed)
