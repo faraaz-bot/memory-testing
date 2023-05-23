029029041e746aece5b09a4532cf794aff94cfa2
Cheyenne Goh <cheyenne.goh@amd.com>
2023-05-23 14:21:33 -0600

    Prototype pointer arithmetic with flat arrays

--- docs/real/code/rcshuffledims.py
+++ docs/real/code/rcshuffledims.py
@@ -6,20 +6,20 @@ import random
 np.set_printoptions(precision=3)
 
 # makes an empty NumPy array with a random stride order
-def array_random_strides(a):
+def array_random_strides(a, seed=10):
     axes = []
     for i in range(len(a)):
         axes.append(i)
-    axes = np.random.permutation(axes)
+    axes = np.random.RandomState(seed=seed).permutation(axes)
     dims = []
     for axis in axes:
         dims.append(a[axis])
-    f = np.empty(dims)
+    f = np.zeros(dims)
     return np.transpose(f, np.argsort(axes))
 
 def rinit_2d(nx, ny, seed=10):
     random.seed(seed)
-    f = array_random_strides([nx, ny])
+    f = array_random_strides([nx, ny], seed)
     for i in range(nx):
         for j in range(ny):
             f[i, j] = random.random()
@@ -27,80 +27,147 @@ def rinit_2d(nx, ny, seed=10):
 
 def rinit_3d(nx, ny, nz, seed=10):
     random.seed(seed)
-    f = array_random_strides([nx, ny, nz])
+    f = array_random_strides([nx, ny, nz], seed)
     for i in range(nx):
         for j in range(ny):
             for k in range(nz):
                 f[i, j, k] = random.random()
     return f 
 
-def impose_expected_format_2d(tdata, nx, ny):
-    sdata = np.empty([nx, ny // 2 + 1], dtype=complex)
+def sort_strides(strides, axes):
+    result = []
+    for axis in axes:
+        result.append(strides[axis])
+    return tuple(result)
+
+def complex_conjugate(x):
+    return x.real - 1j * (x.imag)
+
+def reverse_axes_3d(x):
+    result = [None, None, None]
+    for i in range(len(x)):
+        result[x[i]] = i
+    return result
+
+def impose_expected_format_2d(tdata, tstrides, nx, ny):
+    sdata = np.zeros([nx * (ny // 2 + 1)], dtype=complex)
+    sstrides = [len(sdata) // nx, 
+                len(sdata) // (nx * (ny // 2 + 1))]
 
     for j in range(ny // 2 + 1):
         # fill section with copied values
         for i in range(nx // 2 + 1):
-            sdata[i][j] = tdata[i][j]
+            sdata_idx = i * sstrides[0] + j * sstrides[1]
+            tdata_idx = i * tstrides[0] + j * tstrides[1]
+            sdata[sdata_idx] = tdata[tdata_idx]
         
         # fill remaining with conjugate values
         for i in range(nx // 2 + 1, nx):
-            sdata[i][j] = tdata[nx - i][0 if j == 0 else ny - j].conj()
+            sdata_idx = i * sstrides[0] + j * sstrides[1]
+            tdata_idx = ((nx - i) * tstrides[0] + 
+                         (0 if j == 0 else ny - j) * tstrides[1])
+            sdata[sdata_idx] = complex_conjugate(tdata[tdata_idx])
 
-    return sdata
+    return sdata, sstrides
 
-def impose_expected_format_3d(tdata, nx, ny, nz, axes):
-    sdata = np.empty([nx, ny, nz // 2 + 1], dtype=complex)
-
+def impose_expected_format_3d(tdata, tstrides, nx, ny, nz, axes):
+    sdata = np.zeros([nx * ny * (nz // 2 + 1)], dtype=complex)
+    sstrides = [len(sdata) // nx, 
+                len(sdata) // (nx * ny), 
+                len(sdata) // (nx * ny * (nz // 2 + 1))]
+    
     for k in range(nz // 2 + 1):
         for j in range(ny // 2 + 1):
             # fill section with copied values
             for i in range(nx // 2 + 1):
-                sdata[i][j][k] = tdata[i][j][k]
+                sdata_idx = i * sstrides[0] + j * sstrides[1] + k * sstrides[2]
+                tdata_idx = i * tstrides[0] + j * tstrides[1] + k * tstrides[2]
+                sdata[sdata_idx] = tdata[tdata_idx]
             
             # fill section with copied or conjugate values depending on transpose axes
             for i in range(nx // 2 + 1, nx):
+                sdata_idx = i * sstrides[0] + j * sstrides[1] + k * sstrides[2]
                 if axes[2] == 0:
-                    sdata[i][j][k] = tdata[nx - i][0 if j == 0 else ny - j][0 if k == 0 else nz - k].conj()
+                    tdata_idx = ((nx - i) * tstrides[0] +
+                                 (0 if j == 0 else ny - j) * tstrides[1] +
+                                 (0 if k == 0 else nz - k) * tstrides[2])
+                    sdata[sdata_idx] = complex_conjugate(tdata[tdata_idx])
                 else:
-                    sdata[i][j][k] = tdata[i][j][k]
+                    tdata_idx = (i * tstrides[0] + 
+                                 j * tstrides[1] + 
+                                 k * tstrides[2])
+                    sdata[sdata_idx] = tdata[tdata_idx]
     
         for j in range(ny // 2 + 1, ny):
             # fill section with copied or conjugate values depending on transpose axes
             for i in range(nx // 2 + 1):
+                sdata_idx = i * sstrides[0] + j * sstrides[1] + k * sstrides[2]
                 if axes[2] == 1:
-                    sdata[i][j][k] = tdata[0 if i ==  0 else nx - i][ny - j][0 if k == 0 else nz - k].conj()
+                    tdata_idx = ((0 if i == 0 else nx - i) * tstrides[0] +
+                                 (ny - j) * tstrides[1] +
+                                 (0 if k == 0 else nz - k) * tstrides[2])
+                    sdata[sdata_idx] = complex_conjugate(tdata[tdata_idx])
                 else:
-                    sdata[i][j][k] = tdata[i][j][k]
+                    tdata_idx = (i * tstrides[0] + 
+                                 j * tstrides[1] + 
+                                 k * tstrides[2])
+                    sdata[sdata_idx] = tdata[tdata_idx]
             
             # fill section with copied or conjugate values depending on transpose axes
             for i in range(nx // 2 + 1, nx):
+                sdata_idx = i * sstrides[0] + j * sstrides[1] + k * sstrides[2]
                 if axes[2] == 2:
-                    sdata[i][j][k] = tdata[i][j][k]
+                    tdata_idx = (i * tstrides[0] + 
+                                 j * tstrides[1] + 
+                                 k * tstrides[2])
+                    sdata[sdata_idx] = tdata[tdata_idx]
                 else:
-                    sdata[i][j][k] = tdata[0 if i ==  0 else nx - i][ny - j][0 if k == 0 else nz - k].conj()
+                    tdata_idx = ((0 if i == 0 else nx - i) * tstrides[0] +
+                                 (ny - j) * tstrides[1] +
+                                 (0 if k == 0 else nz - k) * tstrides[2])
+                    sdata[sdata_idx] = complex_conjugate(tdata[tdata_idx])
 
-    return sdata
+    return sdata, sstrides
 
 def test_2d(nx, ny, seed):
     # initialize data
     x = rinit_2d(nx, ny, seed)
 
+    # flatten into 1d array and strides
+    istrides = [s // x.itemsize for s in x.strides]
+    x_flat = x.flatten('K')
+
     # no need to transpose
     if (not nx % 2 == 0 and ny % 2 == 0
-        or x.strides[0] > x.strides[1] and (not nx % 2 == 0 or ny % 2 == 0)):
+        or istrides[0] > istrides[1] and (not nx % 2 == 0 or ny % 2 == 0)):
         return
     
     # transpose directions
-    x_transpose = np.transpose(x)
+    istrides = sort_strides(istrides, [1, 0])
+
+    # reshape into 2d array to execute transform
+    x_transpose = np.lib.stride_tricks.as_strided(
+        x_flat, shape=[ny, nx], strides=[s * x_flat.itemsize for s in istrides])
 
     # execute real-complex transform
     X_transpose = np.fft.rfft2(x_transpose)
 
+    # flatten into 1d array and strides
+    ostrides = [s // X_transpose.itemsize for s in X_transpose.strides]
+    X_transpose_flat = X_transpose.flatten('K')
+
     # transpose back
-    X = np.transpose(X_transpose)
+    ostrides = sort_strides(ostrides, [1, 0])
 
     # impose expected format
-    X_format = impose_expected_format_2d(X, nx, ny)
+    X_flat_format, ostrides = impose_expected_format_2d(
+        X_transpose_flat, ostrides, nx, ny)
+
+    # reshape into 2d array for comparison
+    X_format = np.lib.stride_tricks.as_strided(
+        X_flat_format, 
+        shape=[nx, ny // 2 + 1], 
+        strides=[s * X_flat_format.itemsize for s in ostrides])
 
     # compute oracle
     X_oracle = np.fft.rfft2(x)
@@ -114,17 +181,20 @@ def test_3d(nx, ny, nz, seed):
     # initialize data
     x = rinit_3d(nx, ny, nz, seed)
 
+    # flatten into 1d array and strides
+    istrides = [s // x.itemsize for s in x.strides]
+    x_flat = x.flatten('K')
+    
     dims = [nx, ny, nz]
     axes = [0, 1, 2]
 
     # sort strides in descending order
-    if x.strides[axes[1]] < x.strides[axes[2]]:
+    if istrides[axes[1]] < istrides[axes[2]]:
         axes[1], axes[2] = axes[2], axes[1]
-    if x.strides[axes[0]] < x.strides[axes[2]]:
+    if istrides[axes[0]] < istrides[axes[2]]:
         axes[0], axes[2] = axes[2], axes[0]
-    if x.strides[axes[0]] < x.strides[axes[1]]:
+    if istrides[axes[0]] < istrides[axes[1]]:
         axes[0], axes[1] = axes[1], axes[0]
-
 
     # maximize contiguity for even dimensions
     if (dims[axes[0]] % 2 != 0):
@@ -133,32 +203,50 @@ def test_3d(nx, ny, nz, seed):
     else:
         if (dims[axes[1]] % 2 == 0):
             if (dims[axes[2]] % 2 != 0):
-                axes = np.roll(axes, 1)
+                axes[0], axes[1], axes[2] = axes[2], axes[0], axes[1]
         else:
             if (dims[axes[2]] % 2 == 0):
                 axes[0], axes[1] = axes[1], axes[0]
             else:
-                axes = np.roll(axes, -1)
+                axes[0], axes[1], axes[2] = axes[1], axes[2], axes[0]
 
     # no need to transpose
     if list(axes) == [0, 1, 2]:
         return
  
     # transpose direction
-    x_transpose = np.transpose(x, axes)
+    istrides = sort_strides(istrides, axes)
+
+    # reshape into 3d array to execute transform
+    x_transpose = np.lib.stride_tricks.as_strided(
+        x_flat, 
+        shape=[dims[a] for a in axes], 
+        strides=[s * x_flat.itemsize for s in istrides])
     
     # execute real-complex transform
     X_transpose = np.fft.rfftn(x_transpose)
 
+    # flatten into 1d array and strides
+    ostrides = [s // X_transpose.itemsize for s in X_transpose.strides]
+    X_transpose_flat = X_transpose.flatten('K')
+
     # transpose back
-    X = np.transpose(X_transpose, np.argsort(axes))
+    ostrides = sort_strides(ostrides, reverse_axes_3d(axes))
 
     # impose expected format
-    X_format = impose_expected_format_3d(X, nx, ny, nz, axes)
-
+    X_flat_format, ostrides = impose_expected_format_3d(
+        X_transpose_flat, ostrides, nx, ny, nz, axes)
+    
+    # reshape into 3d array for comparison
+    X_format = np.lib.stride_tricks.as_strided(
+        X_flat_format, 
+        shape=[nx, ny, nz // 2 + 1],
+        strides=[s * X_flat_format.itemsize for s in ostrides])
+    
     # compute oracle
     X_oracle = np.fft.rfftn(x)
 
+    # compare result to oracle
     if not np.allclose(X_oracle, X_format):
         print("Failed", nx, ny, nz, seed)
         exit(0)
