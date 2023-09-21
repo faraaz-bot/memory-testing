004d58984535d77e16de29d6905335b708a48252
Malcolm Roberts <malcolm.roberts@amd.com>
2023-09-21 13:14:08 -0600

    First version of dual amd/nvidia hiprocFFTXt/hipcuFFTXt test

---
+++ eg/mixdev/dual.hpp
@@ -0,0 +1,6 @@
+std::vector<std::vector<std::complex<double>>> cuda_data(const int Nx,
+                                                        const int Ny,
+                                                        std::vector<std::complex<double>> & in);
+std::vector<std::vector<std::complex<double>>> hip_data(const int Nx,
+                                                        const int Ny,
+                                                        std::vector<std::complex<double>> & in);
---
+++ eg/mixdev/hip_side.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <hip/hip_runtime_api.h>
+#include "hipfft/hipfftXt.h"
+
+std::vector<std::vector<std::complex<double>>> hip_data(const int Nx,
+                                                        const int Ny,
+                                                        std::vector<std::complex<double>> & input)
+{
+    std::cout << "cufftXt version\n";    
+
+    // GPUs to use
+    std::vector<int> gpus = {0, 0};
+
+    hipLibXtDesc* desc; // input descriptor
+
+    
+    hipfftHandle plan      = nullptr;
+
+    auto hipfft_rt = hipfftCreate(&plan);
+    if(hipfft_rt != HIPFFT_SUCCESS)
+        throw std::runtime_error("failed to create plan");
+
+    // Define which GPUs are to be used
+     hipfft_rt = hipfftXtSetGPUs(plan, gpus.size(), gpus.data());
+    if(hipfft_rt != HIPFFT_SUCCESS)
+        throw std::runtime_error("hipfftXtSetGPUs failed.");
+
+    // Create the plan
+    size_t workSize[gpus.size()];
+    hipfft_rt = hipfftMakePlan2d(plan, Nx, Ny, HIPFFT_Z2Z, workSize);
+    if(hipfft_rt != HIPFFT_SUCCESS)
+        throw std::runtime_error("hipfftMakePlan2d failed.");
+    
+    // Copy input data to GPUs
+    hipfftXtSubFormat_t format = HIPFFT_XT_FORMAT_INPLACE;
+    hipfft_rt                  = hipfftXtMalloc(plan, &desc, format);
+    if(hipfft_rt != HIPFFT_SUCCESS)
+        throw std::runtime_error("hipfftXtMalloc failed.");
+    
+    hipfft_rt = hipfftXtMemcpy(plan,
+                               reinterpret_cast<void*>(desc),
+                               reinterpret_cast<void*>(input.data()),
+                               HIPFFT_COPY_HOST_TO_DEVICE);
+    if(hipfft_rt != HIPFFT_SUCCESS)
+        throw std::runtime_error("hipfftXtMemcpy H2D failed.");
+
+    // Execute the plan
+    hipfft_rt = hipfftXtExecDescriptor(plan, desc, desc, -1);
+    if(hipfft_rt != HIPFFT_SUCCESS)
+        throw std::runtime_error("hipfftXtExecDescriptor failed.");
+
+
+    std::vector<std::vector<std::complex<double>>> outs(gpus.size());
+    for(int idx = 0; idx < outs.size(); ++idx) {
+        const size_t bufsize = desc->descriptor->size[idx];
+        std::cout << "idx: " << idx << " size: " << bufsize << "\n";
+        const size_t bufcount = bufsize / sizeof(std::complex<double>);
+        outs[idx].resize(bufcount);
+        
+        auto hipret = hipMemcpy(outs[idx].data(),
+                                desc->descriptor->data[idx],
+                                bufsize,
+                                hipMemcpyDeviceToHost);
+        if(hipret != hipSuccess) {
+            std::stringstream ss;
+            ss << "hipMemcpy D2H failed with code ";
+            ss << hipret;
+            throw std::runtime_error(ss.str());
+        }
+    }
+    
+    
+    // FIXME: free things
+    
+    return outs;
+}
