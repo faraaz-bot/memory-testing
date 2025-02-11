c3c947500230a1ea25c350a92f0c677c58208ac4
Kevin Tran <Kevin.Tran@amd.com>
2025-02-11 15:26:32 -0700

    properly concat dataframes and basic text output of avg metrics

--- scripts/perf/multevent/metric_parser.py
+++ scripts/perf/multevent/metric_parser.py
@@ -7,8 +7,8 @@ import argparse
 from io import StringIO
 
 """
-Intended specifically when run with mi300x all .ini file config, with only MALL metrics enabled.
-Extract blocks of format: 
+Tested by running MultEvent profiler with mi300x all .ini file config, with only MALL metrics enabled.
+Extract blocks of following format from within output csv:
 '
 Data Fabric - MALL0
 
@@ -23,7 +23,7 @@ Data Fabric - MALL31
 '
 """
 
-# The interested field and its attributes
+# Metrics of interest
 fields = {
     # Ignore % time metrics, full vs partial hit rate
     "Total Read bandwidth": {"unit": "GB/s", "label": "Read"},
@@ -32,23 +32,6 @@ fields = {
     "Total Write Hit Rate": {"unit": "%", "label": "Write"},
     "Total Read Hit Rate": {"unit": "%", "label": "Read"},
 }
-
-# Group the fields to show details per bank.
-# NB: dataclasses may be better.
-groups = [
-    {
-        "title": "Hit Rate Details (%)",
-        "items": ["Total Hit Rate", "Total Read Hit Rate", "Total Write Hit Rate"],
-        "height": 16,
-        "y_lim": 100,
-    },
-    {
-        "title": "Bandwidth Details (GB/s)",
-        "items": ["Total Read bandwidth", "Total Write bandwidth"],
-        "height": 16,
-        "y_lim": None,
-    },
-]
 
 def plot(dataframes):
     pass
@@ -59,6 +42,7 @@ def parse_csv(filepath, num_columns):
 
     csvs = []
     
+    # Extract each of 32 "MALL sections" into their own csv string and dataframe
     start = False
     curr_csv = ''
     num = -1
@@ -69,37 +53,48 @@ def parse_csv(filepath, num_columns):
                 curr_csv = ''
             start = True
             num = int(l.split('L')[-1]) # Track which MALL # we are on
-            # Add in column headers
-            curr_csv += l + f', GPUDF_0, GPUDF_1, GPUDF_2, GPUDF_3, GPUDF_4, GPUDF_5, GPUDF_6, GPUDF_7, GPUDF_8, GPUDF_9, GPUDF_10, GPUDF_11, GPUDF_12, GPUDF_13, GPUDF_14, GPUDF_15, GPUDF_16, GPUDF_17, GPUDF_18, GPUDF_19, GPUDF_20, GPUDF_21, GPUDF_22, GPUDF_23, GPUDF_24, GPUDF_25, GPUDF_26, GPUDF_27, GPUDF_28, GPUDF_29, GPUDF_30, GPUDF_31\n'
+            # Add in column headers, accounting for current MALL section # for easy merging later
+            header_strings = [f'col_{num * num_columns + n}' for n in range(32)]
+            curr_csv += f'metrics, {", ".join(header_strings)}\n'
             continue
     
         if start:
             if f'MALL{num}' in l:
-                curr_csv += f'{l}\n'
+                # Strip away MALL#
+                stripped_str = l.replace(f'MALL{num}','MALL')
+                curr_csv += f'{stripped_str}\n'
     csvs.append(curr_csv) # Catch last CSV
 
-    # Extract csvs into their own dataframes, including only 5 columns (for MI300X)
+    # Extract csvs into their own dataframes, including only N columns (most columns are unused)
     dataframes = []
-    print(num_columns)
-    for csv in csvs:
-        df = pd.read_csv(StringIO(csv)).iloc[:,:num_columns]
-        dataframes.append(df)
-        # print(df,'\n')
-    
-    print(dataframes[0], '\n')
-    # Adjust df keys to match each other, for merging dfs later
 
+    for i, csv in enumerate(csvs):
+        if i == 0:
+            tmp_df = pd.read_csv(StringIO(csv), dtype=str).iloc[:,0:num_columns]
+        else:
+            tmp_df = pd.read_csv(StringIO(csv), dtype=str).iloc[:,1:num_columns]
+        dataframes.append(tmp_df)
 
-    
+    # Merge together dfs row-wise
+    # Note: column headers get duplicated, not an actual issue though?
+    df = pd.concat(dataframes, axis=1)
+    df = df.apply(lambda x: x.str.strip() if isinstance(x, str) else x).replace(' ', '0')
+
+    field_data = {}
+    # Extract desired metrics
+    for k in fields.keys():
+        field_df = df[df['metrics'].str.contains(k)]
+        field_df.reset_index(drop=True, inplace=True)
+        del field_df['metrics']
+        field_df = field_df.astype(float)
+        field_data[k] = field_df.mean().mean()
+
+    return field_data
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='metric_parser')
-    parser.add_argument("df_file")
-    # parser.add_argument('-s',
-    #                     '--start_line',
-    #                     default=18,
-    #                     type=int,
-    #                     help='Specify the line start to read.')
+    parser.add_argument("csv_files", nargs=argparse.REMAINDER)
 
     # parser.add_argument('-f, --file',
     #                     type=str,
@@ -112,25 +107,10 @@ if __name__ == '__main__':
                         help='Number of columns to take out of 32 (default 5 for MI300X)')
 
     args = parser.parse_args()
-    parse_csv(args.df_file, args.num_columns)
-
-    """
-    TODO:
-    - Print average metrics for key values
-    - Allow comparison against multiple files (either multiple args, or a filepath to glob)
-    """
-
-
-    # if args.command == 'list':
-    #     scprint(set(['function_pool.cpp'] + list_generated_kernels(kernels)))
-#
-    # if args.command == 'generate':
-    #     cpu_functions = generate_kernels(kernels, precisions,
-    #                                      args.stockham_gen)
-    #     func_files = generate_cpu_function_pool_pieces(cpu_functions,
-    #                                                    args.num_files)
-    #     for i in range(args.num_files):
-    #         write(f'function_pool_init_{i}.cpp', func_files[i], format=False)
-    #     write('function_pool.cpp',
-    #           generate_cpu_function_pool_main(args.num_files),
-    #           format=False)
+    results = {} # Map batch size to metrics
+    for file in args.df_file:
+        results[file] = parse_csv(file, args.num_columns)
+        print(file + ':')
+        for k, v in results[file].items():
+            print(f'  {k}\t{v}')
+        print()
