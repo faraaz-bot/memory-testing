1ca78133653b1ace0a7b3133082176aa58048a5c
Kevin Tran <Kevin.Tran@amd.com>
2025-02-11 16:51:59 -0700

    add plotting functionality

--- scripts/perf/multevent/metric_parser.py
+++ scripts/perf/multevent/metric_parser.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import argparse
+import re
 from io import StringIO
 
 """
@@ -24,17 +25,51 @@ Data Fabric - MALL31
 """
 
 # Metrics of interest
+# fields = {
+#     # Ignore % time metrics, full vs partial hit rate
+#     "Total Read bandwidth": {"unit": "GB/s", "label": "Read"},
+#     "Total Write bandwidth": {"unit": "GB/s", "label": "Write"},
+#     "Total Hit Rate": {"unit": "%", "label": "Total"},
+#     "Total Write Hit Rate": {"unit": "%", "label": "Write"},
+#     "Total Read Hit Rate": {"unit": "%", "label": "Read"},
+# }
+
+# Store key metrics of interest, as well as filename for metric
 fields = {
     # Ignore % time metrics, full vs partial hit rate
-    "Total Read bandwidth": {"unit": "GB/s", "label": "Read"},
-    "Total Write bandwidth": {"unit": "GB/s", "label": "Write"},
-    "Total Hit Rate": {"unit": "%", "label": "Total"},
-    "Total Write Hit Rate": {"unit": "%", "label": "Write"},
-    "Total Read Hit Rate": {"unit": "%", "label": "Read"},
+    "Total Read bandwidth": "read_bandwidth",
+    "Total Write bandwidth": "write_bandwidth",
+    "Total Hit Rate": "total_hit_rate",
+    "Total Write Hit Rate": "total_write_hit_rate",
+    "Total Read Hit Rate": "total_read_hit_rate",
 }
 
-def plot(dataframes):
-    pass
+def plot(results, output_path):
+    x_axis = sorted(results.keys()) # Batch sizes
+
+    # For each metric, get y-axis data per file given
+    for field in fields.keys():
+        y_axis = [results[batch][field] for batch in x_axis]
+         
+        plt.clf()
+
+        plt.scatter(x_axis, y_axis, color='blue', s=10, zorder=2, label='')
+        plt.plot(x_axis, y_axis, color='blue')
+
+        # plt.scatter(x_axis, y_axis, color='red', s=10, zorder=3, label='')
+        # plt.plot(x_axis, y_axis, color='red')
+
+        # plt.xscale('log', base=2)
+        # plt.yscale('log', base=2)
+
+        plt.xlabel('Batch Size')
+        y_label = "GB/s" if "bandwidth" in field else "%"
+        plt.ylabel(y_label)
+        plt.title(field)
+        plt.grid()
+        plt.legend()
+        plt.savefig(output_path + f'{fields[field]}.png')  # save the figure
+
 
 def parse_csv(filepath, num_columns):
     with open(filepath, 'r') as f:
@@ -95,22 +130,40 @@ def parse_csv(filepath, num_columns):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='metric_parser')
     parser.add_argument("csv_files", nargs=argparse.REMAINDER)
-
-    # parser.add_argument('-f, --file',
-    #                     type=str,
-    #                     help='Path to csv file to read from.',
-    #                     default="GPUDF.csv")
     
+    # Note: specify these options before files, since csv_files takes remainder of args
     parser.add_argument('-n', '--num_columns', 
                         default=5, 
-                        type=int, 
+                        type=int,
                         help='Number of columns to take out of 32 (default 5 for MI300X)')
 
+    parser.add_argument('-o', '--output_path',
+                        default="./",
+                        help='Directory to store output graphs')
+
+    parser.add_argument('-t', '--text_output',
+                        action="store_true",
+                        help="Flag to enable result output via text")
+
+    parser.add_argument('-g', '--graph_output',
+                        default=True,
+                        action="store_true",
+                        help="Flag to enable disable output via graph")
+
     args = parser.parse_args()
-    results = {} # Map batch size to metrics
-    for file in args.df_file:
-        results[file] = parse_csv(file, args.num_columns)
-        print(file + ':')
-        for k, v in results[file].items():
-            print(f'  {k}\t{v}')
-        print()
+    results = {} # Map batch size to metrics dict
+    for file in args.csv_files:
+        # Extract number found in filename as batch, if present
+        nums_in_file = [int(num) for num in re.findall(r'\d+', file)]
+        batch = nums_in_file[0]
+
+        results[batch] = parse_csv(file, args.num_columns)
+
+        if args.text_output:
+            print(f'Batch Size = {batch}')
+            for k, v in results[batch].items():
+                print(f'  {k}\t{v}')
+            print()
+    
+    if args.graph_output:
+        plot(results, args.output_path)
