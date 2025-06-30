d04acfdfe95917549e01c3039016400228fa7fe4
Faraaz Mustafa <ffaraazm@ctr2-alola-ctrl-01.amd.com>
2025-06-30 17:06:34 -0400

    add build

---
+++ benchs/memcpy/build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/AMD/ffaraazm/rocFFT-misc/benchs/memcpy/build/_deps/googletest-src
+# Build directory: /home/AMD/ffaraazm/rocFFT-misc/benchs/memcpy/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
