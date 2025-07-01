d84c761b43e3dfeee213b48b25dd009714415ed4
Faraaz Mustafa <143056802+faraaz-bot@users.noreply.github.com>
2025-07-01 14:21:51 -0600

    Delete benchs/memcpy/build directory

--- benchs/memcpy/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/AMD/ffaraazm/rocFFT-misc/benchs/memcpy
-# Build directory: /home/AMD/ffaraazm/rocFFT-misc/benchs/memcpy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googlebenchmark-build")
-subdirs("_deps/googletest-build")
--- benchs/memcpy/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/AMD/ffaraazm/rocFFT-misc/benchs/memcpy/build/_deps/googletest-src/googlemock
-# Build directory: /home/AMD/ffaraazm/rocFFT-misc/benchs/memcpy/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
